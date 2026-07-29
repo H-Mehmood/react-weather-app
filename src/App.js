7f2e226ef59e5b3d71f791492720b051d1db7a5e
H-Mehmood <hassanig567@gmail.com>
2026-07-29 17:42:12 +0500

    added dynamically date and location

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
 import logo from './logo.svg';
+import thunder from './img/thunder.png';
 import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [city, setCity] = useState("");
+  const currentDate = new Date().toLocaleDateString("en-US", {
+    month: "short",
+    day: "2-digit",
+    year: "numeric",
+  });
+
+  useEffect(() => {
+    if (!navigator.geolocation) {
+      alert("Geolocation is not supported by your browser.");
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      async (position) => {
+        const { latitude, longitude } = position.coords;
+
+        try {
+          const response = await fetch(
+            `https://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}`
+          );
+
+          const data = await response.json();
+
+          const city =
+            data.address.city ||
+            data.address.town ||
+            data.address.village ||
+            data.address.state;
+
+          setCity(city);
+        } catch (error) {
+          console.error(error);
+        }
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }, []);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1 className="container_date">{currentDate}</h1>
+        <div className="weather_data">
+          <h2 className="container_city">{city}</h2>
+          <img src={thunder} className="container_img" alt="logo" />
+          <h2 className="container_degree">25°C</h2>
+          <h2 className="country_per">sunny</h2>
+          
+        </div>
+      </div>
     </div>
   );
 }
